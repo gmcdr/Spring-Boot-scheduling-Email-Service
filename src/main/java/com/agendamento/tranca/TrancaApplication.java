58609e10c6e0ba3da14f23dc999e7cc78f3dccf0
Gabriel Reis <tharazo101@gmail.com>
2022-01-25 23:25:59 -0300

    First

---
+++ src/main/java/com/agendamento/tranca/TrancaApplication.java
@@ -0,0 +1,13 @@
+package com.agendamento.tranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrancaApplication.class, args);
+	}
+
+}
