9ade4c32beae02311003e73f34825f441c9171f1
Gabriel Reis <tharazo101@gmail.com>
2022-02-09 11:17:07 -0300

    Css update

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,7 +1,33 @@
+function errorMsg(){
+				Swal.fire({
+  				icon: 'error',
+  				title: 'Oops...',
+ 			    text: 'Preencha os campos corretamente!!',
+ 			    confirmButtonColor: '#BB4E26'
+})
+}
+
+function successMsg(){
+				Swal.fire({
+  				icon: 'error',
+  				title: 'Agendamento Confirmado!',
+ 			    text: 'Verifique sua caixa de email!',
+ 			    confirmButtonColor: '#BB4E26'
+})
+}
+
 function complete(){
-	Swal.fire(
-  'Agendamento Confirmado!',
-  'Verifique sua caixa de email!',
-  'success',
-)
+	if(nome.value ==""){
+		errorMsg();
+}else if(email.value ==""){
+		errorMsg();
+}else if(data.value == ""){
+		errorMsg();
+}else if(hora.value == ""){
+	errorMsg();
+}
+else{
+	successMsg();
+}
+		
 }
